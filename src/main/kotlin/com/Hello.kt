d2717f514718a8300becc0ce25e0a359ab71b4dc
B10630321 <48037990+B10630321@users.noreply.github.com>
2021-09-27 11:53:37 +0800

    Initial commit

---
+++ src/main/kotlin/com/Hello.kt
@@ -0,0 +1,15 @@
+package com
+
+fun main(args:Array<String>) {
+//    println("HELLO KOTLIN")
+//    Human().Hello()
+    val h=Human()
+    h.Hello()
+}
+
+class Human{
+    fun Hello(){
+        println("HEllo KT");
+    }
+}
+
